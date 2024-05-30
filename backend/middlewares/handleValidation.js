9b0759516d38c3a7368c342e1616c07d64d4b93c
Samara Caldas <samaracaldass@hotmail.com>
2024-05-30 11:20:22 -0300

    fixing errors

--- backend/middlewares/handleValidation.js
+++ backend/middlewares/handleValidation.js
@@ -1,20 +1,19 @@
-const {validationResult} = require("express-validator")
-
+const { validationResult } = require("express-validator");
 
 const validate = (req, res, next) => {
+  const errors = validationResult(req);
 
-    const errors = validationResult(req)
+  if (errors.isEmpty()) {
+    return next();
+  }
 
-    if(errors.isEmpty()) {
-        return next()
-    }
+  const extractedErrors = [];
 
-    const extractedErros = []
+  errors.array().map((err) => extractedErrors.push(err.msg));
 
-    errors.array().map((err) => extractedErros.push(err.msg))
-
-    return res.status(422).json({
-        errors: extractedErros,
-    });
-
+  return res.status(422).json({
+    errors: extractedErrors,
+  });
 };
+
+module.exports = validate;
--- backend/routes/Router.js
+++ backend/routes/Router.js
@@ -2,6 +2,5 @@ const express = require("express");
 const router = express();
 
 router.use("/api/users", require("./UserRoutes"));
-router.use("/api/photos", require("./PhotoRoutes"));
 
 module.exports = router;
