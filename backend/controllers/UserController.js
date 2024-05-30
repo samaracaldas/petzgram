ca930f6f36f9364dec1f5d825d3374261ca0c9ae
Samara Caldas <samaracaldass@hotmail.com>
2024-05-30 16:00:27 -0300

    user login

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -51,8 +51,31 @@ const register = async(req, res) => {
 };
 
 // sign user in
-const login = (req, res) => {
-    res.send("Login");
+const login = async (req, res) => {
+    
+    const {email, password} = req.body
+
+    const user = await User.findOne({email})
+
+    // check if user exists
+    if(!user) {
+        res.status(404).json({errors: ["Usuário não encontrado."]})
+        return
+    }
+
+    // check if password matches
+    if(!(await bcrypt.compare(password, user.password))) {
+        res.status(422).json({errors: ["Senha inválida."]})
+        return
+    }
+
+    // return user with token
+    res.status(201).json({
+        _id: user._id,
+        profileImage: user.profileImage,
+        token: generateToken(user._id),
+    })
+
 }
 
 module.exports = {
