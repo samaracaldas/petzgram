b436e9a9af16f76743b29a99557af53c2c9812de
Samara Caldas <samaracaldass@hotmail.com>
2024-06-03 18:15:45 -0300

    user update middleware

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -2,19 +2,34 @@ const express = require("express")
 const router = express.Router()
 
 // controller
-const { register, login, getCurrentUser } = require("../controllers/UserController");
+const { 
+    register, 
+    login, 
+    getCurrentUser,
+    update,
+ } = require("../controllers/UserController");
 
 // middlewares
 const validate = require("../middlewares/handleValidation");
 const { 
     userCreateValidation, 
-    loginValidation 
+    loginValidation, 
+    userUpdateValidation
 } = require("../middlewares/userValidations");
 const authGuard = require("../middlewares/authGuard");
+const { imageUpload } = require("../middlewares/imageUpload");
 
 // routes
 router.post("/register", userCreateValidation(), validate, register);
 router.post("/login", loginValidation(), validate, login);
-router.get("/profile", authGuard, getCurrentUser)
+router.get("/profile", authGuard, getCurrentUser);
+router.put(
+    "/", 
+    authGuard, 
+    userUpdateValidation(), 
+    validate, 
+    imageUpload.single("profileImage"),
+    update
+);
 
 module.exports = router;
