f21d1f23f9b3a3bc4d00a8a434942708237bae49
Samara Caldas <samaracaldass@hotmail.com>
2024-06-10 15:07:46 -0300

    getting user by id

---
+++ backend/controllers/PhotoController.js
@@ -0,0 +1,18 @@
+const Photo = require("../models/Photo")
+
+const mongoose = require("mongoose")
+
+// insert a photo, with an user related to it
+const insertPhoto = async(req, res) => {
+
+    const {title} = req.body;
+    const image = req.file.filename;
+
+    console.log(req.body);
+
+    res.send("Photo insert");
+};
+
+module.exports = {
+    insertPhoto,
+}
